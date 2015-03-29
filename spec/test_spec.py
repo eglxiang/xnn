640357e8b306e1546a89fd132046e5781ec06b98
Josh Susskind <josh@emotient.com>
2015-03-29 02:58:39 -0700

    clean up test_spec and add some commented code for drawing graphs

--- spec/test_spec.py
+++ spec/test_spec.py
@@ -3,6 +3,11 @@ from spec.layers.base import *
 import pprint
 
 
+BATCHSIZE=128
+IMGWIDTH=48
+IMGHEIGHT=48
+IMGDIMS=IMGWIDTH*IMGHEIGHT
+
 # -------------------------------------------------------------------- #
 # First setup the data channel sets for supervised learning
 # -------------------------------------------------------------------- #
@@ -20,10 +25,13 @@ emos.add(Channel("contempt", negative_weight=.1, channel_weight=.9))
 # Basic mlp with softmax layers
 # -------------------------------------------------------------------- #
 mlp = Model()
-pixels = mlp.add(InputLayer(shape=(48,48)))
-hidden1 = mlp.add(DenseLayer(parent=pixels.name, num_units=100, nonlinearity=rectify()))
-hidden2 = mlp.add(DenseLayer(parent=hidden1.name, num_units=100, nonlinearity=rectify()))
-labels = mlp.add(DenseLayer(parent=hidden2.name, num_units=aus.size(), nonlinearity=softmax()))
+pixels = mlp.add(InputLayer(shape=(BATCHSIZE, IMGDIMS)), name="pixels")
+hidden1 = mlp.add(DenseLayer(parent=pixels.name, num_units=100,
+                             nonlinearity=rectify()), name="hidden1")
+hidden2 = mlp.add(DenseLayer(parent=hidden1.name, num_units=200,
+                             nonlinearity=rectify()), name="hidden2")
+labels = mlp.add(DenseLayer(parent=hidden2.name, num_units=aus.size(),
+                            nonlinearity=softmax()), name="labels")
 
 mlp.add_channel_set(aus)
 
@@ -46,9 +54,11 @@ pprint.pprint(mlp.instantiate())
 # Basic nonlinear autoencoder (without weight-sharing)
 # -------------------------------------------------------------------- #
 autoencoder = Model()
-pixels = autoencoder.add(InputLayer(shape=(48,48)))
-hidden = autoencoder.add(DenseLayer(parent=pixels.name, num_units=100, nonlinearity=tanh()))
-recon = autoencoder.add(DenseLayer(parent=hidden.name, num_units=48*48, nonlinearity=linear()))
+pixels = autoencoder.add(InputLayer(shape=(BATCHSIZE,IMGDIMS)), name="pixels")
+hidden = autoencoder.add(DenseLayer(parent=pixels.name, num_units=100,
+                                    nonlinearity=tanh()), name="hidden")
+recon = autoencoder.add(DenseLayer(parent=hidden.name, num_units=IMGDIMS,
+                                   nonlinearity=linear()), name="recon")
 
 autoencoder.bind_output(
     layername=recon.name,
@@ -68,14 +78,19 @@ pprint.pprint(autoencoder.instantiate())
 # Joint nonlinear autoencoder+mlp (without weight-sharing)
 # -------------------------------------------------------------------- #
 joint = Model()
-pixels = joint.add(InputLayer(shape=(48,48)))
-hid1up = joint.add(DenseLayer(parent=pixels.name, num_units=100, nonlinearity=rectify()))
-encoder = joint.add(DenseLayer(parent=hid1up.name, num_units=100, nonlinearity=rectify()))
-hid1down = joint.add(DenseLayer(parent=encoder.name, num_units=100, nonlinearity=rectify()))
+pixels = joint.add(InputLayer(shape=(BATCHSIZE,IMGDIMS)), name="pixels")
+hid1up = joint.add(DenseLayer(parent=pixels.name, num_units=100,
+                              nonlinearity=rectify()), name="hid1up")
+encoder = joint.add(DenseLayer(parent=hid1up.name, num_units=100,
+                               nonlinearity=rectify()), name="encoder")
+hid1down = joint.add(DenseLayer(parent=encoder.name, num_units=100,
+                                nonlinearity=rectify()), name="hid1down")
 # This is the pixel reconstruction layer
-recon = joint.add(DenseLayer(parent=hid1down.name, num_units=48*48, nonlinearity=linear()))
+recon = joint.add(DenseLayer(parent=hid1down.name, num_units=IMGDIMS,
+                             nonlinearity=linear()), name="recon")
 # This is the label layer for supervised learning
-labels = joint.add(DenseLayer(parent=encoder.name, num_units=emos.size(), nonlinearity=softmax()))
+labels = joint.add(DenseLayer(parent=encoder.name, num_units=emos.size(),
+                              nonlinearity=softmax()), name="labels")
 
 joint.add_channel_set(emos)
 
@@ -110,33 +125,33 @@ pprint.pprint(joint.instantiate())
 # -------------------------------------------------------------------- #
 semi = Model()
 pixels = semi.add(
-    InputLayer(shape=(48,48)))
+    InputLayer(shape=(BATCHSIZE,IMGDIMS)), name="pixels")
 hid1up = semi.add(
     DenseLayer(parent=pixels.name,
                num_units=100,
-               nonlinearity=rectify()))
+               nonlinearity=rectify()), name="hid1up")
 # Use sigmoid labels (like we do in practice)
 labelencoder = semi.add(
     DenseLayer(parent=hid1up.name,
                num_units=emos.size(),
-               nonlinearity=sigmoid()))
+               nonlinearity=sigmoid()), name="labelencoder")
 # Make hidden encoder also use sigmoid for consistency
 hidencoder = semi.add(
     DenseLayer(parent=hid1up.name,
                num_units=100,
-               nonlinearity=sigmoid()))
+               nonlinearity=sigmoid()), name="hidencoder")
 # This is the joint encoder layer with label predictions and hidden units
 jointencoder = semi.add(
-    ConcatLayer(parents=[labelencoder.name, hidencoder.name]))
+    ConcatLayer(parents=[labelencoder.name, hidencoder.name]), name="jointencoder")
 hid1down = semi.add(
     DenseLayer(parent=jointencoder.name,
                num_units=100,
-               nonlinearity=rectify()))
+               nonlinearity=rectify()), name="hid1down")
 # This is the pixel reconstruction layer
 recon = semi.add(
     DenseLayer(parent=hid1down.name,
-               num_units=48*48,
-               nonlinearity=linear()))
+               num_units=IMGDIMS,
+               nonlinearity=linear()), name="recon")
 
 semi.add_channel_set(emos)
 
@@ -168,47 +183,14 @@ pprint.pprint(semi.instantiate())
 
 
 #
+# import os
+# from draw_net.draw_net import *
 #
+# nettypes = ['mlp', 'autoencoder', 'joint', 'semi']
+# for nettype in nettypes:
+#     mynet = eval(nettype)
+#     layers=mynet.instantiate()
+#     # draw_to_notebook(layers, verbose=True, output_shape=False)
+#     draw_to_file(layers, "/tmp/%s.png" % nettype, verbose=True, output_shape=False)
+#     os.system('open /tmp/%s.png' % nettype)
 #
-# #
-# # m2 = modelSpec()
-# # l0 = m2.add(inputLayerSpec(shape=(48,48)))
-# # l1 = m2.add(denseLayerSpec(parent=l0.name, **b_props))
-# # l2 = m2.add(concatLayerSpec(parents=[l0.name, l1.name]))
-# # l3 = m2.add(denseLayerSpec(parent=l2.name, num_units=100))
-# #
-# # m2.bind_output(layername=l3.name,
-# #                settings=outputSpec(
-# #                    loss=crossentropySpec(),
-# #                    eval_output_activation=linearSpec(), scale=1.0,
-# #                    target=ReconstructionTargetSpec(layer=m2.last().name)))
-# #
-#
-# # -------------------------------------------------------------------- #
-#
-# # out_props = outputSpec(loss=crossentropySpec(), eval_output_activation=linearSpec(), scale=1.0, target=ChannelsTargetSpec(separator=None))
-# #
-# #
-# # # using first and last functions
-# # m3 = modelSpec()
-# # m3.add(inputLayerSpec(shape=(48,48)))
-# # m3.add(denseLayerSpec(parent=m3.first(), **b_props))
-# # m3.add(concatLayerSpec(parents=[m3.first(), m3.last()]))
-# # m3.add(denseLayerSpec(parent=m3.last(), num_units=100), output_settings=out_props)
-# #
-# # # using explicit reference by layer name (where names are auto-generated)
-# # m4 = modelSpec()
-# # m4.add(inputLayerSpec(shape=(48,48)))
-# # m4.add(denseLayerSpec(parent=m4.get_layer("inputLayer_0"), **b_props))
-# # m4.add(concatLayerSpec(parents=[m4.get_layer("inputLayer_0"), m4.get_layer("denseLayer_1")]))
-# # m4.add(denseLayerSpec(parent=m4.get_layer("concatLayer_2"), num_units=100), output_settings=out_props)
-# #
-# # # using explicit reference by layer name (where names are manually specified)
-# # m5 = modelSpec()
-# # m5.add(inputLayerSpec(shape=(48,48)), name="inputLayer_0")
-# # m5.add(denseLayerSpec(parent=m5.get_layer("inputLayer_0"), **b_props), name="denseLayer_1")
-# # m5.add(concatLayerSpec(parents=[m5.get_layer("inputLayer_0"), m5.get_layer("denseLayer_1")]), name="concatLayer_2")
-# # m5.add(denseLayerSpec(parent=m5.get_layer("concatLayer_2"), num_units=100), name="denseLayer_3", output_settings=out_props)
-# #
-# #
-# # assert m1.to_dict()==m2.to_dict()==m3.to_dict()==m4.to_dict()==m5.to_dict()
