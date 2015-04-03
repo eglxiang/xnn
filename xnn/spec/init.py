fa187175f3fb3bc97bf20e335da16470eda055f3
Max Anger <manger@emotient.com>
2015-04-02 17:28:09 -0700

    Normal, Sparse, and Orthogonal initializers added

--- xnn/spec/init.py
+++ xnn/spec/init.py
@@ -33,3 +33,40 @@ class Constant(Initializer):
 
     def instantiate(self):
         return lasagne.init.Constant(val=self.val)
+
+class Normal(Initializer):
+    def __init__(self, std=0.01, avg=0.0, **kwargs):
+        if not isinstance(std, Number):
+            raise TypeError("std must be a number.")
+        if not isinstance(avg, Number):
+            raise TypeError("avg must be a number.")
+        super(Normal, self).__init__(**kwargs)
+        self.std = std
+        self.avg = avg
+
+    def instantiate(self):
+        return lasagne.init.Normal(std=self.std, avg=self.avg)
+
+class Sparse(Initializer):
+    def __init__(self, std=0.01, sparsity=0.1, **kwargs):
+        if not isinstance(std, Number):
+            raise TypeError("std must be a number.")
+        if not isinstance(sparsity, Number):
+            raise TypeError("sparsity must be a number.")
+        super(Sparse, self).__init__(**kwargs)
+        self.std = std
+        self.sparsity = sparsity
+
+    def instantiate(self):
+        return lasagne.init.Sparse(std=self.std, avg=self.avg)
+
+class Orthogonal(Initializer):
+    def __init__(self, gain=1.0, **kwargs):
+        if not isinstance(gain, Number) and gain!='relu':
+            raise TypeError("Gain must be a number or 'relu'.")
+
+        super(Orthogonal, self).__init__(**kwargs)
+        self.gain = gain
+
+    def instantiate(self):
+        return lasagne.init.Orthogonal(gain=self.gain)
