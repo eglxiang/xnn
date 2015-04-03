2ae836028638f2a255dc26c8756a5ec9e919592c
Max Anger <manger@emotient.com>
2015-04-02 17:28:42 -0700

    LeakyRectify instantiate method added

--- xnn/spec/nonlinearities.py
+++ xnn/spec/nonlinearities.py
@@ -52,5 +52,4 @@ class LeakyRectify(Activation):
         self.leakiness = leakiness
 
     def instantiate(self):
-        #TODO: instantiate LeakyRectify
-        return None
+        return lasange.nonlinearities.LeakyRectify(leakiness=self.leakiness)
