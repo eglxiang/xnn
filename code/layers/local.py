3370e1559e73c36f2c2665f30bfe1aeb19e6d1dc
Josh Susskind <josh@emotient.com>
2015-06-24 15:01:00 -0700

    add layers folder with contrast norm layer and layers test

---
+++ code/layers/local.py
@@ -0,0 +1,16 @@
+# import numpy as np
+# import theano.tensor as T
+#
+# from lasagne import init
+# from lasagne import nonlinearities
+
+from lasagne.layers.base import Layer
+
+
+__all__ = [
+    "LocalLayer"
+]
+
+
+class LocalLayer(Layer):
+    pass
