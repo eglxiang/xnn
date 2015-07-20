e3c242a377482d96d90daa986f7cc28699a12537
Walter Talbott <walter@emotient.com>
2015-07-20 13:16:28 -0700

    loop saves best model for each metric

--- training/loop.py
+++ training/loop.py
@@ -4,7 +4,7 @@ from bokeh.plotting import output_server, figure, push, curdoc, cursession,vplot
 import time
 
 class Loop(object):
-    def __init__(self,trainer,learndata=[],validdata=[],metricsdict={},url=None,savefilename=None,weightdict={},printflag=True,plotmetricmean=True):
+    def __init__(self,trainer,learndata=[],validdata=[],metricsdict={},url=None,savefilenamecsv=None,weightdict={},printflag=True,plotmetricmean=True,savemodelnamebase=None):
         """
         An example training loop.  The trainer will learn from learndata, apply metrics to validdata, plot to bokeh server at url if supplied, and save to csv file.
         :param trainer: The trainer to run
@@ -12,10 +12,11 @@ class Loop(object):
         :param validdata:  A list of generator functions that yield batches of data to which the metrics will be applied
         :param metricsdict:  A dictionary of metrics to apply to the validdata.  The keys of metricsdict are the keys in the model.predict output from which the metric should be calculated
         :param url: A string representing the url where the bokeh server is running (e.g. 'http://127.0.0.1:5006'). If None, or if server cannot be connected, no plotting will occur
-        :param savefilename:  The name of a file into which to write metric results at each epoch.
+        :param savefilenamecsv:  The name of a file into which to write metric results at each epoch.
         :param weightdict:  Dictionary of Weighters.  Keys in weightdict are keys into which the weight results will be inserted in the data dictionary.  Weights are calculated for every training batch.
         :param printflag: Whether to print results to stdout after each epoch
         :param plotmetricmean: Whether to print/save/plot mean of all metrics.  Depending on the metrics, this value might not make sense.
+        :param savemodelnamebase: Base name for saving metrics.  If not None, models will be saved for the best values of each metric as looping procedes.
         """
         self.trainer = trainer
         self.learndata = self._listify(learndata)
@@ -27,13 +28,20 @@ class Loop(object):
             self._datasources = None
         else:
             self._plot_flag = False
-        if savefilename is not None:
-            self.savefilename=savefilename
-            self._save_flag = True
+        if savefilenamecsv is not None:
+            self.savefilenamecsv=savefilenamecsv 
+            self._savecsv_flag = True
         else:
-            self._save_flag = False
+            self._savecsv_flag = False
+        if savemodelnamebase is not None:
+            self.savemodelnamebase = savemodelnamebase
+            self._savemodel_flag = True
+        else:
+            self._savemodel_flag = False
         self._print_flag = printflag
         self._plotmetricmean=plotmetricmean
+        self._bestmetvals = None
+        self._bestatep = None
         self.meandur = None
         self.ep = 0
         
@@ -69,6 +77,9 @@ class Loop(object):
             else:
                 self.meandur = 0.3*self.meandur + 0.7*dur
 
+            #update best values
+            isbest = self._update_best(ep,trainerr,metvals)
+
             #print summary to stdout
             if self._print_flag:
                 self._print(ep,trainerr,metvals,dur,endep)
@@ -78,8 +89,14 @@ class Loop(object):
                 self._plot(ep,trainerr,metvals)
 
             #save to csv
-            if self._save_flag:
+            if self._savecsv_flag:
                 self._save(ep,trainerr,metvals)
+
+            #save model if best for any metrics
+            isbest_nontrainerr = isbest[1:]
+            if self._savemodel_flag and any(isbest_nontrainerr):
+                self._savemodel(isbest_nontrainerr)
+
         if self._print_flag:
             print('Finished %d epochs at %s'%(niter,time.strftime('%I:%M:%S %p')))
 
@@ -87,6 +104,43 @@ class Loop(object):
         for k,w in self.weightdict.iteritems():
             batch[k] = w(batch)
         return batch
+
+    def _savemodel(self,isbest):
+        if self._plotmetricmean:
+            if isbest[0]:
+                fname = self.savemodelnamebase + '_metricmean'
+                self.trainer.model.save_model(fname)
+            isbest = isbest[1:] 
+        for mk,ib in zip(self.metricsdict.keys(),isbest):
+            if ib:
+                fname = self.savemodelnamebase + '_' + mk
+                self.trainer.model.save_model(fname)
+    
+    def _update_best(self,ep,trainerr,metvals):
+        isbest = []
+        best = []
+        bestatep = []
+        if self._plotmetricmean:
+            vals = [trainerr] + [np.mean(metvals)] + metvals
+        else:
+            vals = [trainerr]+metvals
+        if self._bestmetvals is None:
+            self._bestmetvals = vals
+            self._bestatep = [ep]*(len(vals))
+            isbest = [True]*(len(vals))
+        else:
+            for v,b,e in zip(vals,self._bestmetvals,self._bestatep):
+                if v < b:
+                    isbest.append(True)
+                    best.append(v)
+                    bestatep.append(ep)
+                else:
+                    isbest.append(False)
+                    best.append(b)
+                    bestatep.append(e)
+            self._bestmetvals = best
+            self._bestatep = bestatep
+        return isbest
 
     def _plot(self,ep,trainerr,metvals):
         if self._datasources is None:
@@ -128,19 +182,21 @@ class Loop(object):
         push()
 
     def _print(self,ep,trainerr,metvals,dur,totalep):
-        fmt = '{0:45} {1:20}:   {2:0.4f}'
+        fmt = '{0:45} {1:20}:   {2:0.4f} (best {3:0.4f} at epoch {4:g})'
         print '=========='
         print('Epoch %d / %d -- %0.2f seconds'%(ep,totalep-1,dur))
         print('Expected Finish: %s'%(self._timedone(ep,totalep)))
         print '----------'
-        print(fmt.format('Training total cost','',trainerr))
+        print(fmt.format('Training total cost','',trainerr,self._bestmetvals[0],self._bestatep[0]))
         print '----------'
+        beststartid = 1
         if self._plotmetricmean:
-            print fmt.format('Overall Mean','',np.mean(metvals))
+            print fmt.format('Overall Mean','',np.mean(metvals),self._bestmetvals[1],self._bestatep[1])
             print '----------'
-        for mv,(mk,m) in zip(metvals,self.metricsdict.iteritems()):
+            beststartid = 2
+        for mv,(mk,m),bv,be in zip(metvals,self.metricsdict.iteritems(),self._bestmetvals[beststartid:],self._bestatep[beststartid:]):
             name = xnn.metrics.metric_names[m.metric]
-            print fmt.format(name,mk,mv)
+            print fmt.format(name,mk,mv,bv,be)
             
     def _timedone(self,ep,totalep):
         now = time.time()
@@ -152,7 +208,7 @@ class Loop(object):
         if (not hasattr(self,'_headers_written')) or (not self._headers_written):
             self._save_header()
             self._headers_written = True
-        with open(self.savefilename,'a') as f:
+        with open(self.savefilenamecsv,'a') as f:
             f.write(('%0.4f,'%trainerr))
             if self._plotmetricmean:
                 f.write(('%0.4f,'%np.mean(metvals)))
@@ -162,7 +218,7 @@ class Loop(object):
             f.flush()
     
     def _save_header(self):
-        with open(self.savefilename,'a') as f:
+        with open(self.savefilenamecsv,'a') as f:
             f.write('Training cost,')
             if self._plotmetricmean:
                 f.write('Overall mean,')
