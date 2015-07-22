10ec9301dda8693da0687bcf7c16855a1e2071f6
Josh Susskind <josh@emotient.com>
2015-07-22 15:57:23 -0700

    comment experiment

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -9,7 +9,21 @@ MAXCONDITIONSINMEMORY = 10000
 
 
 class ExperimentCondition(object):
+    """
+    The :class:`ExperimentCondition` class represents a structure holding experiment variable values.
+
+    This class is used to parameterize experiment conditions.
+    """
+
     def to_dict(self):
+        """
+        Returns a JSON-serializable python dictionary representing experiment condition variables.
+
+        Returns
+        -------
+        dictionary of native python types
+            A dictionary holding experiment condition variables
+        """
         properties = deepcopy(self.__dict__)
         for key in properties:
             properties[key] = _convert_property_value(properties[key])
@@ -17,13 +31,39 @@ class ExperimentCondition(object):
 
 
 class Experiment(object):
+    """
+    The :class:`Experiment` class manages specifying and iterating through an experiment design.
+
+    It is intended to specify factors whose values define models (e.g. neural nets) and to
+    manage iterating, querying, and reporting on particular experimental conditions.
+    """
+
     def __init__(self, id, default_condition):
+        """
+        Instantiates the experiment.
+
+        Parameters
+        ----------
+        id : a string or None
+            An optional experiment ID.
+        default_condition : a :class:`ExperimentCondition` instance
+            The experiment condition object defining experiment variables.
+        """
         self.id = id
         self.factors = dict()
         self.default_condition = default_condition
         self.conditions = None
 
     def to_dict(self):
+        """
+        Returns a JSON-serializable python dictionary representing the experiment specification.
+
+        Returns
+        -------
+        dictionary of native python types
+            A dictionary with keys "factors" and "default_condition" holding the specified
+            factors and condition information.
+        """
         factors = dict()
         for factorkey in self.factors:
             for item in self.factors[factorkey]:
@@ -36,6 +76,16 @@ class Experiment(object):
         )
 
     def add_factor(self, name, levels):
+        """
+        Adds a factor to the experiment specifying levels of a particular condition to vary.
+
+        Parameters
+        ----------
+        name : a string that must match one of the keys in the default_condition member variable
+            the name of a key in default_condition whose values are to be varied.
+        levels : a list of appropriate types (not type-safe)
+            the values that define the levels of the factor
+        """
         if name not in self.default_condition.__dict__:
             errstr = "name %s is not a valid property. Must be one of these:\n\t%s" \
                 % (name, '\n\t'.join(self.default_condition.to_dict().keys()))
@@ -44,27 +94,82 @@ class Experiment(object):
         self._generate_conditions()
 
     def get_num_conditions(self):
+        """
+        Returns the number of experiment conditions (the cross-product of factor levels)
+
+        Returns
+        -------
+        int
+            An integer specifying the number of experiment conditions in the experiment.
+        """
         if self.factors:
             return reduce(mul, [len(self.factors[factor]) for factor in self.factors], 1)
         else:
             return 0
 
-    def get_nth_condition_changes(self, n, default=None, conditions=None):
+    def get_nth_condition_changes(self, n, conditions=None):
+        """
+        Returns a dictionary containing the experiment variables for a particular condition
+        that differ from the set of values specified in default_condition.
+
+        Parameters
+        ----------
+        n : an int
+            the condition number to query
+        conditions : an optional itertools.product instance
+            cartesian product of experiment factors.
+            If not specified uses the conditions generated for the specified experiment.
+        Returns
+        -------
+        dict
+            A dictionary where keys/values are experiment condition variable names and values
+            for those items that are based on specified factors.
+        """
         conditions_ = self.conditions if conditions is None else conditions
-        condition = next(islice(conditions_, n, None), default)
+        condition = next(islice(conditions_, n, None), {})
         # print n
-        condition = dict(condition)
+        condition = dict(condition) if condition is not None else {}
         # TODO: allow maintaining position in generator rather than resetting
         self._generate_conditions()
         return condition
 
-    def get_nth_condition(self, n, default=None, conditions=None):
+    def get_nth_condition(self, n, conditions=None):
+        """
+        Returns a :class:`ExperimentCondition` instance containing the experiment variables for a particular condition.
+
+        Parameters
+        ----------
+        n : an int
+            the condition number to query
+        conditions : an optional itertools.product instance
+            cartesian product of experiment factors.
+            If not specified uses the conditions generated for the specified experiment.
+
+        Returns
+        -------
+        A :class:`ExperimentCondition` instance
+            Contains the experiment variables for a particular condition.
+        """
         conditions_ = self.conditions if conditions is None else conditions
-        cond = next(islice(conditions_, n, None), default)
+        cond = next(islice(conditions_, n, None), {})
         self._generate_conditions()
         return self._patch_condition(cond)
 
     def get_conditions_iterator(self, start=0, stop=None):
+        """
+        Returns an iterator over experiment conditions between start and end condition indices.
+
+        Parameters
+        ----------
+        start : an int (default 0)
+            A condition index to start the iterator.
+        stop : an int or None (Default None)
+            A condition index to end the iterator (None means continue through all conditions).
+
+        Returns
+        -------
+        An iterator which iterates over :class:`ExperimentCondition` instances in the experiment.
+        """
         stop = self.get_num_conditions() if stop is None else stop
         for i in range(start, stop):
             yield self.get_nth_condition(i)
