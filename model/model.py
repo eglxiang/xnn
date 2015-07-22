797f84e0fa4a81fe159d0f94c92d638eb43e3708
Walter Talbott <walter@emotient.com>
2015-07-22 14:46:49 -0700

    add more comments to model

--- model/model.py
+++ model/model.py
@@ -8,16 +8,16 @@ from xnn import layers
 
 __all__=['Model']
 
-"""
-The Model class stores the layers and connectivity structure of the model, and
-provides utility functions for building a network from lasagne layers.  The
-Model also provides methods for associating network inputs and outputs with
-fields in the data dictionary.  One advantage of using the Model class over
-lasagne layers is that the Model provides serialization and de-serialization.
-
-"""
 
 class Model(object):
+    """
+    The Model class stores the layers and connectivity structure of the model, and
+    provides utility functions for building a network from lasagne layers.  The
+    Model also provides methods for associating network inputs and outputs with
+    fields in the data dictionary.  One advantage of using the Model class over
+    lasagne layers is that the Model provides serialization and de-serialization.
+
+    """
 
     def __init__(self,name='model'):
         self.name    = name
@@ -28,15 +28,16 @@ class Model(object):
         self._predict_func = None
         self._unique_name_counters = dict()
 
-    """
-    Add a layer to the model.  Layers should be added in a topological order: all inputs to a layer (unless it is an input layer) should be added to the model first.
-
-    :param layer: A lasagne layer object
-    :param name: The name of the layer is used to index the output of that layer in the data returned from the predict method.  Although names are generated automatically, it is generally a good idea to keep track of the names of layers whose output is important.
-
-    """
 
     def add_layer(self,layer,name=None):
+        """
+        Add a layer to the model.  Layers should be added in a topological order: all inputs to a layer (unless it is an input layer) should be added to the model first.
+
+        :param layer: A lasagne layer object
+        :param name: The name of the layer is used to index the output of that layer in the data returned from the predict method.  Although names are generated automatically, it is generally a good idea to keep track of the names of layers whose output is important.
+
+        :return: The lasagne layer object that was added to the network.
+        """
         if name is None and layer.name is not None:
             name = self._get_unique_name_from_layer(layer)
         if layer.name is None and name is not None:
@@ -52,6 +53,11 @@ class Model(object):
         return layer
 
     def add_full_net_from_layer(self,outlayer):
+        """
+        Create a model from a single output layer.  This will not work for multi-output models.
+
+        :param outlayer: A lasagne layer object.
+        """
         layers = xnn.layers.get_all_layers(outlayer)
         for l in layers:
             self.add_layer(l)
@@ -74,6 +80,16 @@ class Model(object):
             return namebase
 
     def make_dropout_layer(self,parentlayer,p=0.5,name=None,drop_type='standard'):
+        """
+        Convenience function to both construct a lasagne dropout layer and add it to the model.
+
+        :param parentlayer: The lasagne layer object whose outputs are the input to the dropout layer
+        :param p: The dropout parameter.  Depending on the dropout type, this parameter means different things.  For standard dropout, this is the percent of neurons to mask.  For Gaussian dropout, this is the variance of the gaussian noise.
+        :param name: The name of the dropout layer.
+        :param drop_type: Either 'standard or 'gauss' for standard and Gaussian dropout.
+        
+        :return: The lasagne layer object that was added to the network.
+        """
         if drop_type == 'standard':
             droplayer  = xnn.layers.DropoutLayer(parentlayer,p=p)
         elif drop_type == 'gauss':
@@ -87,6 +103,16 @@ class Model(object):
         return droplayer
 
     def make_dense_layer(self,parentlayer,num_units,nonlinearity=None,name=None):
+        """
+        Convenience function to both construct a lasagne dense (fully-connected) layer and add it to the model.
+
+        :param parentlayer: The lasagne layer object whose outputs are the input to the dense layer.
+        :param num_units: The number of units in the dense layer.
+        :param nonlinearity: The nonlinearity function to use on this layer.  If None, :py:meth:`xnn.nonlinearities.rectify` is used.
+        :param name: The name of the dense layer.
+
+        :return: The lasagne layer object that was added to the network.
+        """
         if nonlinearity is None:
             nonlinearity = xnn.nonlinearities.rectify
         denselayer = xnn.layers.DenseLayer(parentlayer,num_units=num_units,nonlinearity=nonlinearity)
@@ -97,6 +123,16 @@ class Model(object):
         return denselayer
 
     def make_bound_input_layer(self,shape,inputlabelkey,name=None,input_var=None):
+        """
+        Create an input layer, add it to the model, and bind its input to a field in the data.
+
+        :param shape: The shape of the input (as a tuple) that this layer should expect.
+        :param inputlabelkey: The key in the data dictionary whose value should be fed to the constructed input layer.
+        :param name: The name of the input layer
+        :param input_var: The theano symbol that represents the input to this layer in theano's symbolic graph.  If None, a symbol is created internally.
+        
+        :return: The lasagne layer object that was added to the network.
+        """
         lin = xnn.layers.InputLayer(shape,input_var=input_var,name=name)
         if name is None:
             name = self._get_unique_name_from_layer(lin)
@@ -106,6 +142,18 @@ class Model(object):
         return lin
 
     def make_dense_drop_stack(self,parent_layer,num_units_list=None,drop_p_list=None,nonlin_list=None,namebase=None,drop_type_list=None):
+        """
+        Create a sequence of dense layers followed by dropout layers.
+
+        :param parent_layer: The lasagne layer object whose outputs are the input to the first dense layer.
+        :param num_units_list: A list of integers specifying the number of units.  The length of this list is the number of dense layers that will be added to the model.
+        :param drop_p_list: A list of parameters for the dropout layers.  The length should be the same as :py:attr:`num_units_list`, :py:attr:`drop_type_list`, and :py:attr:`nonlin_list`.  If None, all values are 0.5.
+        :param nonlin_list: A list of nonlinearities to apply to the dense layers.  If None, all nonlinearities will be :py:meth:`xnn.nonlinearities.rectify`
+        :param namebase: Base name to prepend to dense and dropout layer names.
+        :param drop_type_list: A list of strings specifying either 'standard' or 'gauss' for the dropout types of each layer in the stack. If None, all types are 'standard'.
+        
+        :return: The lasagne layer object of the final layer added to the network by this method.
+        """
         pl = parent_layer
         # if namebase is None:
         #     namebase="l_"
@@ -131,6 +179,13 @@ class Model(object):
         return pl
 
     def bind_input(self, input_layer, input_key):
+        """
+        Specifies that a particular model layer should be given input from the data dictionary.
+
+        :param input_layer: The lasagne layer object to which to bind the input.
+        :param input_key: The key in the data dictionary whose value will be passed to the input layer
+
+        """
         if not isinstance(input_key, str):
             raise Exception("input_key must be a string")
         if not isinstance(input_layer, xnn.layers.InputLayer):
@@ -139,6 +194,19 @@ class Model(object):
         self.inputs[input_key].append(input_layer)
 
     def bind_output(self, output_layer, loss_function, target, target_type='label', aggregation_type='mean', scale=1.0, is_eval_output=False, weight_key=None):
+        """
+        Specifies that a particular model layer should be treated as an output, which data key holds the labels for this output, and the cost function used for this output.
+        
+        :param output_layer: The lasagne layer object that will be treated as an output.
+        :param loss_function: The function that will be applied to this output and the target to compute the cost.
+        :param target: The key in the data dictionary whose value is the target for this output, or the name of the layer whose output should be reconstructed.
+        :param target_type: Either 'label', for when the target is a field in the data dictionary or 'recon', when the target is the output of another layer.
+        :param aggregation_type: How to aggregate the cost across the examples in a batch.  Must be one of these values: 'mean', 'sum', 'weighted_mean', 'weighted_sum', 'nanmean', 'nansum', 'nanweighted_mean', 'nanweighted_sum'
+        :param scale: Scale the cost for this output by a constant.
+        :param is_eval_output: Specify whether this output should be added to a list of evaluation outputs.
+        :param weight_key: Key in the data dictionary whose value is the weight of each example in the batch for this output.  Only specified if :py:attr:`aggregation_type` is a weighted type.
+
+        """
         aggregation_types = ['mean', 'sum', 'weighted_mean','weighted_sum','nanmean','nansum','nanweighted_mean','nanweighted_sum']
         target_types = ['label', 'recon']
         if aggregation_type not in aggregation_types:
@@ -166,6 +234,13 @@ class Model(object):
             self.bind_eval_output(output_layer,target)
 
     def bind_eval_output(self, output_layer, target):
+        """
+        Add an output layer to a list of evaluation outputs.
+
+        :param output_layer: The layer object whose output will be considered an evaluation output.
+        :param target: The key in the data dictionary whose value is the target for this output.
+
+        """
         if output_layer.name not in self.layers:
             raise Exception("Can only bind eval outputs to layers that exist in the model.")
         self.eval_outputs[output_layer.name]=dict(
@@ -174,6 +249,10 @@ class Model(object):
         )
 
     def to_dict(self):
+        """
+        Return a dictionary that represents this model.
+
+        """
         d = {}
         ls = []
         for lname,l in self.layers.iteritems():
@@ -220,6 +299,11 @@ class Model(object):
         return d
 
     def from_dict(self,indict):
+        """
+        Add layers, inputs, and outputs to the current model from a dictionary representation.
+
+        :param indict: A dictionary representation of a model.
+        """
         import copy
         indict = copy.deepcopy(indict)
         self._build_layers_from_list(indict['layers'])
@@ -228,6 +312,12 @@ class Model(object):
         self._bind_eval_outputs_from_list(indict['eval_outputs'])
 
     def from_dict_static(indict): 
+        """
+        Construct a model from a dictionary representation
+
+        :param indict: A dictionary representation of a model.
+        :return: A model build from the dictionary.
+        """
         m = Model(indict['name'])
         m._build_layers_from_list(indict['layers'])
         m._bind_inputs_from_list(indict['inputs'])
@@ -341,6 +431,11 @@ class Model(object):
             return None
 
     def save_model(self,fname):
+        """
+        Save model to a file.
+
+        :param fname: The filename into which the model will be saved.
+        """
         d = self.to_dict()
         all_layers = [self.layers[k] for k in self.layers.keys()]
         p = xnn.layers.get_all_param_values(all_layers)
@@ -349,6 +444,11 @@ class Model(object):
             cPickle.dump(m,f,cPickle.HIGHEST_PROTOCOL)
 
     def load_model(self,fname):
+        """
+        Load model from a file.
+
+        :param fname: The filename that contains the model to be loaded.
+        """
         with open(fname,'rb') as f:
             d = cPickle.load(f)
         self.from_dict(d['model'])
@@ -383,6 +483,13 @@ class Model(object):
         self._predict_func = f
 
     def predict(self,data_dict,layer_names=None,data_in_gpu=False):
+        """
+        Run data through a model and collect the output.
+
+        :param data_dict: A dictionary of data on which the model will run.  This dictionary must contain keys whose values were bound to the input layers of the model.
+        :param layer_names: A list of layer names for which the output will be returned.  If None, output of all layers will be returned
+        :param data_in_gpu: If True, data is on the gpu and the values in :py:attr:`data_dict` are the theano shared variables that contain the data.  If False, the values in :py:attr:`data_dict` are numpy arrays containing the data
+        """
         f = self._predict_func
         if f is None:
             self._get_predict(data_dict,data_in_gpu)
