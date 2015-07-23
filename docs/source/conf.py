d50d15ff014ba6491c2bc43999d1a7a6877c2f64
Max Anger <manger@mbp-manger.emotient.local>
2015-07-23 13:06:48 -0700

    Changed conf.py so that __call__ documentation is shown, but __init__ is not

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,10 +115,8 @@ pygments_style = 'sphinx'
 todo_include_todos = True
 
 
-
-
 def skip(app, what, name, obj, skip, options):
-    if (name == "__init__") or (name == "__call__"):
+    if name == "__call__":
         return False
     return skip
 
